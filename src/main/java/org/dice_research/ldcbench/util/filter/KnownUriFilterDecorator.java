166d96479d066dc45f5487d9326c96214e1d12d3
gsjunior <gsjunior@localhost>
2019-10-13 00:29:30 +0200

    delete unused classes

--- src/main/java/org/dice_research/ldcbench/util/filter/KnownUriFilterDecorator.java
+++
@@ -1,9 +0,0 @@
-package org.dice_research.ldcbench.util.filter;
-
-
-import java.io.Closeable;
-
-public interface KnownUriFilterDecorator extends KnownUriFilter, Closeable {
-
-    public KnownUriFilter getDecorated();
-}
